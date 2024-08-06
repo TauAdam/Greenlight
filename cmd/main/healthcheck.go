3de5a548f7a5ce80d2391946899eb1eb4321d18a
Miras <101412464+TauAdam@users.noreply.github.com>
2024-08-06 15:17:16 +0500

    Update healthcheck.go

--- cmd/main/healthcheck.go
+++ cmd/main/healthcheck.go
@@ -22,24 +22,3 @@ app.logger.Println(err)
 http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 }
 }
-
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-
-js, err := json.Marshal(data)
-if err != nil {
-return err
-}
-
-
-js = append(js, '\n')
-
-for key, value := range headers {
-w.Header()[key] = value
-}
-
-w.Header().Set("Content-Type", "application/json")
-w.WriteHeader(status)
-w.Write(js)
-
-return nil
-}
