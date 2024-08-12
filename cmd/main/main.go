53978069c17abf45cf0bd82c3259711694a8aebc
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-12 23:37:04 +0500

    feat: use get method && fix errors

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -19,7 +19,10 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn                string
+		maxOpenConnections int
+		maxIdleConnections int
+		maxIdleTime        string
 	}
 }
 
@@ -34,8 +37,8 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
-	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
-	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.IntVar(&cfg.db.maxOpenConnections, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConnections, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
--- cmd/main/movies.go
+++ cmd/main/movies.go
@@ -1,76 +1,81 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"github.com/TauAdam/Greenlight/internal/data"
+	"github.com/TauAdam/Greenlight/internal/validator"
 	"net/http"
 )
 
 func (app *application) handleCreateMovie(w http.ResponseWriter, r *http.Request) {
 	var input struct {
+		Title string `json:"title"`
 
-Title string `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+	// Use the new readJSON() helper to decode the request body into the input struct.
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// validation step
 
-Year int32 `json:"year"`
-Runtime data.Runtime `json:"runtime"`
-Genres []string `json:"genres"`
-}
-// Use the new readJSON() helper to decode the request body into the input struct.
-err := app.readJSON(w, r, &input)
-if err != nil {
-app.badRequestResponse(w, r, err)
-return
-}
-// validation step
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
 
-movie := &data.Movie{
-Title: input.Title,
-Year: input.Year,
-Runtime: input.Runtime,
-Genres: input.Genres,
-}
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
 
-v := validator.New()
-if data.ValidateMovie(v, movie); !v.Valid() {
-app.failedValidationResponse(w, r, v.Errors)
-return
-}
+	err = app.models.Movies.Insert(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
 
-err = app.models.Movies.Insert(movie)
-if err != nil {
-app.serverErrorResponse(w, r, err)
-return
-}
+	// include a Location header to let the
+	// client know which URL they can find the newly-created resource at
+	headers := make(http.Header)
+	headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
 
-// include a Location header to let the
-// client know which URL they can find the newly-created resource at
-headers := make(http.Header)
-headers.Set("Location", fmt.Sprintf("/v1/movies/%d", movie.ID))
-
-// Write a JSON response with a 201 code
-err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
-if err != nil {
-app.serverErrorResponse(w, r, err)
-}
+	// Write a JSON response with a 201 code
+	err = app.writeJSON(w, http.StatusCreated, envelope{"movie": movie}, headers)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
 
 // handleShowMovie retrieve the interpolated "id" parameter from the current URL
 func (app *application) handleShowMovie(w http.ResponseWriter, r *http.Request) {
-id, err := app.readIDParam(r)
-if err != nil {
-http.NotFound(w, r)
-return
-}
-movie := data.Movie{
-ID: id,
-CreatedAt: time.Now(),
-Title: "Casablanca",
-Runtime: 102,
-Genres: []string{"drama", "romance", "war"},
-Version: 1,
-}
+	id, err := app.extractIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
 
-err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-if err != nil {
-   app.serverErrorResponse(w, r, err)
-}
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
