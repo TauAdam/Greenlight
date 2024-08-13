dda41555e2be6c0b5f1cfeb87073c85779a18a48
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-13 18:23:21 +0500

    feat: helper functions

--- cmd/main/helper.go
+++ cmd/main/helper.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/TauAdam/Greenlight/internal/validator"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 )
 
 // extractIDParam read id parameter from the current URL
@@ -17,52 +23,88 @@ func (app *application) extractIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-
 type envelope map[string]interface{}
 
 func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
-js, err := json.MarshalIndent(data, "", "\t")
-if err != nil {
-return err
-}
-js = append(js, '\n')
-for key, value := range headers {
-w.Header()[key] = value
-}
-w.Header().Set("Content-Type", "application/json")
-w.WriteHeader(status)
-w.Write(js)
-return nil
+	js, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	js = append(js, '\n')
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(js)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	err := json.NewDecoder(r.Body).Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+			// check eof error
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
 
-// Decode the request body
-err := json.NewDecoder(r.Body).Decode(dst)
-if err != nil {
-var syntaxError *json.SyntaxError
-var unmarshalTypeError *json.UnmarshalTypeError
-var invalidUnmarshalError *json.InvalidUnmarshalError
-switch {
-case errors.As(err, &syntaxError):
-return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
-// check eof error 
-case errors.Is(err, io.ErrUnexpectedEOF):
-return errors.New("body contains badly-formed JSON")
-
-case errors.As(err, &unmarshalTypeError):
-if unmarshalTypeError.Field != "" {
-return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+			// A json.InvalidUnmarshalError error will be returned if we pass a non-nil
+			// pointer to Decode().
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		default:
+			return err
+		}
+	}
+	return nil
 }
-return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
 
-case errors.Is(err, io.EOF):
-return errors.New("body must not be empty")
-// A json.InvalidUnmarshalError error will be returned if we pass a non-nil
-// pointer to Decode().
-case errors.As(err, &invalidUnmarshalError):
-panic(err)
+// The readString returns a string value from the query string
+func (app *application) readString(queryStr url.Values, key, defaultValue string) string {
+	// If no key exists this will return the empty string "".
+	s := queryStr.Get(key)
+	if s == "" {
+		return defaultValue
+	}
 
-default:
-return err
+	return s
+}
+
+func (app *application) readCSV(queryStr url.Values, key string, defaultValue []string) []string {
+	csv := queryStr.Get(key)
+	if csv == "" {
+		return defaultValue
+	}
+
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInteger(queryStr url.Values, key string, defaultValue int, v *validator.Validator) int {
+	s := queryStr.Get(key)
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
