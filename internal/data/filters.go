3e590eba4671dbdfc5f565c14a14380151b04023
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-08-14 14:23:43 +0500

    feat: pagination

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"github.com/TauAdam/Greenlight/internal/validator"
+	"math"
 	"strings"
 )
 
@@ -12,6 +13,7 @@ type Filters struct {
 	SortOptions []string
 }
 
+// sortColumn returns the column name we should use to sort the database query results.
 func (f Filters) sortColumn() string {
 	for _, safeValue := range f.SortOptions {
 		if f.Sort != safeValue {
@@ -28,6 +30,14 @@ func (f Filters) sortDirection() string {
 	return "ASC"
 }
 
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
 func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.Page > 0, "page", "must be greater than zero")
 	v.Check(f.Page <= 10_000, "page", "must be a maximum of 10 thousands")
@@ -36,4 +46,27 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortOptions...), "sort", "invalid sort value")
+}
+
+type Metadata struct {
+	FirstPage    int `json:"first_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	CurrentPage  int `json:"current_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+}
+
+// calculateMetadata calculates the metadata for a paginated response.
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		FirstPage:    1,
+		PageSize:     pageSize,
+		CurrentPage:  page,
+		TotalRecords: totalRecords,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+	}
 }
